e61c4251b8cd870e4dd6b0c2bdea2910ba9d5b4c
ripple_k <ripple_k@163.com>
2020-08-06 17:47:07 +0800

    init project

---
+++ Follow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Follow
+//
+//  Created by 张坤 on 2020/8/4.
+//  Copyright © 2020 张坤. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Follow/RootViewController.swift
@@ -0,0 +1,49 @@
+//
+//  RootViewController.swift
+//  Follow
+//
+//  Created by 张坤 on 2020/8/4.
+//  Copyright © 2020 张坤. All rights reserved.
+//
+
+import SwiftUI
+
+class RootViewController<Content: View>: UIHostingController<Content> {
+    // MARK: Constants
+    
+    private enum Metric {
+    }
+    
+    private enum Font {
+    }
+    
+    private enum Color {
+    }
+    
+    
+    // MARK: Properties
+    
+    
+    // MARK: UI
+    
+    
+    // MARK: Initializing
+    
+    
+    // MARK: View Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    // MARK: Binding
+    
+    
+}
+
+struct RootViewController_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
