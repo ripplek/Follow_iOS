9843c95f4549305e2f94085ac65804d132d9507c
ripple_k <ripple_k@163.com>
2020-08-24 19:14:38 +0800

    add calculator

---
+++ Follow/CalculatorButtonItem.swift
@@ -0,0 +1,62 @@
+//
+//  CalculatorButtonItem.swift
+//  Follow
+//
+//  Created by 张坤 on 2020/8/24.
+//  Copyright © 2020 张坤. All rights reserved.
+//
+
+import SwiftUI
+
+enum CalculatorButtonItem {
+    enum Op: String {
+        case plus = "+"
+        case minus = "-"
+        case multiply = "x"
+        case divide = "/"
+        case equal = "="
+    }
+    
+    enum Command: String {
+        case clear = "AC"
+        case flip = "+/-"
+        case percent = "%"
+    }
+    
+    case digit(Int)
+    case dot
+    case op(Op)
+    case command(Command)
+}
+
+extension CalculatorButtonItem {
+    var title: String {
+        switch self {
+        case .digit(let value):
+            return String(value)
+        case .dot:
+            return "."
+        case .op(let op):
+            return op.rawValue
+        case .command(let command):
+            return command.rawValue
+        }
+    }
+    
+    var size: CGSize {
+        return CGSize(width: 88, height: 88)
+    }
+    
+    var backgroundColorName: String {
+        switch self {
+        case .digit, .dot:
+            return "digitBackground"
+        case .op:
+            return "operatorBackground"
+        case .command:
+            return "commandBackground"
+        }
+    }
+}
+
+extension CalculatorButtonItem: Hashable {}
--- Follow/ContentView.swift
+++ Follow/ContentView.swift
@@ -9,13 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    // MARK: Constants
+    
+    let row: [CalculatorButtonItem] = [.digit(1), .digit(2), .digit(3), .op(.plus)]
+    
     var body: some View {
-        Text("Hello, World!")
+        
+        HStack {
+            CalculatorButtonRow(row: [.digit(0), .dot, .op(.equal)])
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct CalculatorButton: View {
+    let fontSize: CGFloat = 38
+    let title: String
+    let size: CGSize
+    let backgroundColorName: String
+    let action: () -> Void
+    
+    var body: some View {
+        Button(action: action) {
+            Text(title)
+                .font(.system(size: fontSize))
+                .foregroundColor(.white)
+                .frame(width: size.width, height: size.height)
+                .background(Color(backgroundColorName))
+                .cornerRadius(size.width / 2)
+        }
+    }
+}
+
+struct CalculatorButtonRow: View {
+    let row: [CalculatorButtonItem]
+    
+    var body: some View {
+        ForEach(row, id: \.self) { item in
+            CalculatorButton(
+                title: item.title,
+                size: item.size,
+                backgroundColorName: item.backgroundColorName) {
+                    print(item.title)
+            }
+        }
     }
 }
